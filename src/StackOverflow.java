ff815fa2647385984b4a285a8076386db8b3d9fa
Your Name <you@example.com>
2023-07-11 18:24:41 +0300

    first commit

---
+++ src/StackOverflow.java
@@ -0,0 +1,13 @@
+public class StackOverflow {
+    private static int count;
+    public static void main(String[] args) {
+       f();
+    }
+    public static void f() {
+          count++;
+        if (count % 100 == 0) {
+            System.out.println(" count: " + count);
+            f();
+        }
+    }
+}
